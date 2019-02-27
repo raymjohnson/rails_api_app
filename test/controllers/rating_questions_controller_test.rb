14cef05407ee789db85d7fbf8689de7de88a7283
Rachel Johnson <rachel@squareyes.info>
2019-02-27 15:10:26 +1100

    Fix RatingQuestionsControllerTest class name

--- test/controllers/rating_questions_controller_test.rb
+++ test/controllers/rating_questions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class rating_questionsControllerTest < ActionDispatch::IntegrationTest
+class RatingQuestionsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get rating_questions_index_url
     assert_response :success
